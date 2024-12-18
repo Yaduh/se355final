022ed6da74e2cf9ff9d106d80bd79086e42d99a4
Yaduh <137622535+Yaduh@users.noreply.github.com>
2024-12-19 02:14:14 +0300

    Simple push pull implemented. Done for the night. Rest of push pull and pub sub tomorrow.

---
+++ src/main/java/se355final/ZMQPushPull/ClientSF.java
@@ -0,0 +1,18 @@
+package se355final.ZMQPushPull;
+
+import org.zeromq.*;
+
+public class ClientSF {
+    public static void main(String...args) throws Exception{
+     try(ZContext context = new ZContext()){
+         ZMQ.Socket push = context.createSocket(SocketType.PUSH);
+         push.connect("tcp://localhost:5000"); // Connect to Server at port 5000
+         String msg = "a";
+         
+             System.out.println("Message" + msg + " sent to Server.");
+             push.send(msg);
+             Thread.sleep(1000); // Delay to keep the client connected long enough for the server to receive the message(NOT NECESSARY IN EXAM)
+     }
+    }
+    
+}
---
+++ src/main/java/se355final/ZMQPushPull/ServerRF.java
@@ -0,0 +1,17 @@
+package se355final.ZMQPushPull;
+
+import org.zeromq.*;
+
+public class ServerRF {
+    public static void main(String...args) throws Exception{
+    try(ZContext context = new ZContext()){ 
+        ZMQ.Socket pull = context.createSocket(SocketType.PULL); 
+        pull.bind("tcp://*:5000"); // Bind to port 5000
+        
+        System.out.println("awdawd");
+            byte [] reply = pull.recv(); // Wait for client to connect
+            System.out.println("Received from Client: "+ new String(reply));
+    }
+}
+    
+}
